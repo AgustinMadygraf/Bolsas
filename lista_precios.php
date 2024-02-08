df932dab27bff5697d049109f0328b149ee07426
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-02-08 12:41:36 -0300

    Changed the page title to reflect the content of the page. (generated by blackbox.ai )

--- lista_precios.php
+++ lista_precios.php
@@ -12,7 +12,7 @@ $fechas = getArraySQL($sqlFechas);
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Listado de Precios</title> <!-- Cambié el título para reflejar el contenido de la página -->
+    <title>Listado de Precios</title> 
     <link rel="stylesheet" href="CSS/style.css"> 
 </head>
 <body>
@@ -30,39 +30,36 @@ $fechas = getArraySQL($sqlFechas);
 </form>
 
 <?php
-if (isset($_GET['fechaSeleccionada'])) {
-    $fechaSeleccionada = $_GET['fechaSeleccionada'];
-    $sql = "SELECT t1.ID_formato, t1.formato, t1.color, t1.gramaje, lp.cantidad, lp.precio_u_sIVA 
-            FROM listado_precios lp 
-            INNER JOIN tabla_1 t1 ON lp.ID_formato = t1.ID_formato 
-            WHERE lp.fecha_listado = '$fechaSeleccionada'
-            ORDER BY color DESC";
-    $resultados = getArraySQL($sql);
-    ?>
-    <table border="1" class="responsive-table">
-        <tr>
-            <th>ID_formato</th>
-            <th>Formato</th>
-            <th>Color</th>
-            <th>Gramaje</th>
-            <th>Cantidad</th>
-            <th>Precio Unitario s/IVA</th>
-        </tr>
-        <?php
-        foreach ($resultados as $resultado) {
-            echo "<tr>
-                    <td>{$resultado['ID_formato']}</td>  <!-- acá está vacío en lugar de ver el ID_formato -->          
-                    <td>{$resultado['formato']}</td>
-                    <td>{$resultado['color']}</td>
-                    <td>{$resultado['gramaje']}</td>
-                    <td>{$resultado['cantidad']}</td>
-                    <td>{$resultado['precio_u_sIVA']}</td>
-                  </tr>";
-        }
-        ?>
-    </table>
-    <?php
-}
+
+$fechaSeleccionada = $_GET['fechaSeleccionada'] ?? '2024-02-01';
+$sql = "SELECT t1.ID_formato, t1.formato, t1.color, t1.gramaje, lp.cantidad, lp.precio_u_sIVA 
+        FROM listado_precios lp 
+        INNER JOIN tabla_1 t1 ON lp.ID_formato = t1.ID_formato 
+        WHERE lp.fecha_listado = '$fechaSeleccionada'
+        ORDER BY color DESC";
+$resultados = getArraySQL($sql);
 ?>
+<table border="1" class="responsive-table">
+    <tr>
+        <th>ID_formato</th>
+        <th>Formato</th>
+        <th>Color</th>
+        <th>Gramaje</th>
+        <th>Cantidad</th>
+        <th>Precio Unitario s/IVA</th>
+    </tr>
+    <?php
+    foreach ($resultados as $resultado) {
+        echo "<tr>
+                <td>{$resultado['ID_formato']}</td>
+                <td>{$resultado['formato']}</td>
+                <td>{$resultado['color']}</td>
+                <td>{$resultado['gramaje']}</td>
+                <td>{$resultado['cantidad']}</td>
+                <td>{$resultado['precio_u_sIVA']}</td>
+              </tr>";
+    }
+    ?>
+</table>
 </body>
 </html>
