6ad8bb6f132436a673a017c3c3f85544d0dfb709
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-03-05 16:12:22 -0300

    Update KPI data in `datos.php` (generated by blackbox.ai )

--- includes/datos.php
+++ includes/datos.php
@@ -5,9 +5,7 @@ $data = [
         ["Descripción" => "Energía marginal",       "Valor unitario" =>    "50", "Unidad" => "$/kWh",   "KPI" => "0.0011979386792453",  "Unidad KPI" => "kWh/bolsa"],
         ["Descripción" => "Gluer marginal",         "Valor unitario" =>     "0", "Unidad" => "$/kg",    "KPI" => "0",                   "Unidad KPI" => "kg/bolsa"]
     ];   
-    visualizarTabla($data);
 
-    echo "<h2>Costo fijo - Electrico</h2>";
     $data2 = [ 
         ["Descripción" => "Energía máquina",    "Valor unitario" =>  "50", "Unidad" => "$/kWh", "Potencia" => "252.4",    "Horas por día" => "24",               "Días por mes" => "30"],
         ["Descripción" => "Energía Compresor",    "Valor unitario" =>  "50", "Unidad" => "$/kWh", "Potencia" => "400",    "Horas por día" => "8",               "Días por mes" => "20"],
