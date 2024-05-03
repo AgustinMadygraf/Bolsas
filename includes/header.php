8df360f1d02ea7e0aab0cf774b62f508bb5af681
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-05-03 20:49:29 -0300

    Update header.php to include link for Valor Unitario page

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@ echo "<li><a href='/Bolsas/ventas.php' ".($paginaActual == 'Bolsas/ventas.php' ?
 echo "<li><a href='/Bolsas/stock.php' ".($paginaActual == 'Bolsas/stock.php' ? $claseActiva : "").">Stock</a></li>";
 echo "<li><a href='/Bolsas/lista_precios.php' ".($paginaActual == 'Bolsas/lista_precios.php' ? $claseActiva : "").">Lista Precios</a></li>";
 echo "<li><a href='/Bolsas/manijas.php' ".($paginaActual == 'Bolsas/manijas.php' ? $claseActiva : "").">Manijas</a></li>";
+echo "<li><a href='/Bolsas/valor_unitario.php' ".($paginaActual == 'Bolsas/valor_unitario.php' ? $claseActiva : "").">Valor Unitario</a></li>";
 echo "<li><a href='/Bolsas/presupuesto.php?peso=' ".($paginaActual == 'Bolsas/presupuesto.php?peso=' ? $claseActiva : "").">Presupuesto</a></li>";
 echo "<li><a href='/Bolsas/proveedores.php' ".($paginaActual == 'Bolsas/proveedores.php' ? $claseActiva : "").">Proveedores</a></li>";
 echo "<li><a href='/horas/index.php' ".($paginaActual == '/horas/index.php' ? $claseActiva : "").">Horas</a></li>";
---
+++ valor_unitario.php
@@ -0,0 +1,6 @@
+<!--Bolsas/presupuestos.php-->
+<?php
+require "includes/header.php";
+require 'includes/conn_bolsas.php';
+include 'includes/db_functions.php'; 
+?>
